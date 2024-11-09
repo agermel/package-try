a0dc75c8312a6758402cdf7ec92f8ec85877997f
Agermel <agermel@foxmail.com>
2024-11-09 10:37:22 +0800

    fix

--- 11_9_homework/2/channel.go
+++ 11_9_homework/2/channel.go
@@ -1,15 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"sync"
+)
+
+type Answer struct {
+	ID     int
+	Random int
+}
+
+var Answers []Answer
 
 func main() {
-	ch := make(chan int, 5)
-	go func() {
-		for i := 0; i < 10; i++ {
-			ch <- i
-		}
-	}()
-	for n := range ch {
-		fmt.Println(n)
+	ch := make(chan Answer, 20)
+	var wg sync.WaitGroup
+
+	wg.Add(20)
+	for i := 0; i < 20; i++ {
+		random := rand.Intn(1145)
+		go func(id, random int) {
+			defer wg.Done()
+			ch <- Answer{ID: i, Random: random}
+		}(i, random)
+	}
+	wg.Wait()
+	close(ch)
+
+	for r := range ch {
+		Answers = append(Answers, r)
+		fmt.Printf("Unsorted Goroutine Index: %d, Random: %d\n", r.ID, r.Random)
+	}
+
+	fmt.Println("————————————————————————————————————————————————————————————")
+
+	sort.SliceStable(Answers, func(i, j int) bool {
+		return Answers[i].ID < Answers[j].ID
+	})
+
+	for _, r := range Answers {
+		fmt.Printf("Sorted Goroutine Index: %d, Random: %d\n", r.ID, r.Random)
 	}
 }
---
+++ 11_9_homework/go.mod
@@ -0,0 +1,3 @@
+module 11_9_homework
+
+go 1.23.2
